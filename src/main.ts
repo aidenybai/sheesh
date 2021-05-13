da5405584af6d7146c01081bf8d64833a31bbc25
Aiden Bai <mail.contraband@gmail.com>
2021-05-12 22:43:27 -0700

    Add watercss

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,7 @@
-import './style.css';
+import 'water.css/out/water.css';
+import './style.scss';
+import { component } from 'lucia';
+
+const App = component({ number: 0 });
+
+App.mount('#app');
--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,7 @@
-import { defineConfig } from 'vite';
-import createImportPlugin from 'vite-plugin-import';
 import { minifyHtml } from 'vite-plugin-html';
-import { viteSingleFile } from 'vite-plugin-singlefile';
 
-export default defineConfig({
-  plugins: [
-    createImportPlugin([
-      {
-        libraryName: 'lucia',
-      },
-    ]),
-    viteSingleFile(),
-    minifyHtml(),
-  ],
-});
+const config = {
+  plugins: [minifyHtml()],
+};
+
+export default config;
